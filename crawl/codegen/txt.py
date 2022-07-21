8ba189f9ee933f74f6dceaea702719500ec74c89
inclyc <me@inclyc.cn>
2022-07-22 00:07:37 +0800

    Add chapter split for txt, make iReader happy

--- crawl/codegen/txt.py
+++ crawl/codegen/txt.py
@@ -1,11 +1,18 @@
 import os
+import re
 
 
 def generate(out, titles, contents):
     if not os.path.exists(os.path.dirname(out)):
         os.makedirs(os.path.dirname(out))
+    cnt = 1
     with open(out, 'w') as f:
         for title in titles:
-            f.write(title)
-            f.write('\n')
+            wtitle = title
+            if re.match(r'第.*话', title):
+                wtitle = re.sub(r'第.*话', '第{}章'.format(cnt), title)
+                cnt += 1
+
+            f.write(wtitle)
+            f.write('\n\n\n')
             f.write(contents[title])
